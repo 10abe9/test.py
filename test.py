9b775a93135c3d1ed4f45d7a0c40a02fc33b6ac9
10abe9 <mdaushmda@mail.ru>
2021-09-17 21:59:14 +0300

    дискриминант

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from math import sqrt
 a = 4 #int(input))
 b = 8 #int(input))
-c = 1 #int(input))
+c = 2 #int(input))
 x1=0
 x2=0
 Dis=0
 x1 = -b + sqrt(b**2-4*a*c)
 x2 = -b - sqrt(b**2-4*a*c)
 Dis = b**2-4*a*c
-if Dis == 0:
+if Dis < 0:
 	 print ("net korney")
 else:
 
